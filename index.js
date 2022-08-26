0430db9c65329bb6ca78a4032878748bb512dd8f
djamel-eddine <djamelmahrougui@gmail.com>
2022-08-26 10:28:51 +0100

    create http server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello i'm here");
+const { createServer } = require("http");
+
+const server = createServer((req, res) => {
+  res.end("hello from the inside\n");
+});
+
+const port = 3000;
+
+server.listen(port, () =>
+  console.log(`server is running on port ${port} properly`)
+);
