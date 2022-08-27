2b362e2deacaa1f8435f84c4e5edb847cc1ff4db
djamel-eddine <djamelmahrougui@gmail.com>
2022-08-27 15:58:17 +0100

    add ping route

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const { createServer } = require("http");
+const url = require("url");
+
+//ahndle the environment variables
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const server = createServer((req, res) => {
-  res.end("hello from the inside\n");
+  const { pathname } = url.parse(req.url, true);
+  const { method } = req;
+
+  const handlerPath = pathname.replace("/", "");
+  const chosenHandler = handlers[handlerPath] || handlers.error;
+
+  chosenHandler({}, (code) => {
+    res.statusCode = code;
+    if (code !== 404) {
+      res.setHeader("Content-Type", "application/json");
+      res.end(
+        JSON.stringify({
+          message:
+            "hello from the inside:\n ========" +
+            pathname +
+            " ======== " +
+            method +
+            "========" +
+            code,
+          code,
+          method,
+          pathname,
+        })
+      );
+    } else {
+      res.end();
+    }
+  });
 });
 
-const port = 3000;
+const handlers = {
+  ping: (data, callback) => {
+    callback(200);
+  },
+  error: (_, callback) => {
+    callback(404);
+  },
+};
+
+const port = process.env.PORT;
 
 server.listen(port, () =>
   console.log(`server is running on port ${port} properly`)
